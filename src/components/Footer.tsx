22c2e4aaa6a65bfe4ca535e784074f48ad4f271c
Fernando Barrera <barrerafernando54@gmail.com>
2025-10-25 21:23:12 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -66,7 +66,7 @@ export const Footer = () => {
         </div>
 
         <div className="footer-bottom">
-          <p>&copy; 2024 MotoStore. All rights reserved.</p>
+          <p>&copy; {new Date().getFullYear()} MotoStore. All rights reserved.</p>
         </div>
       </div>
     </footer>
